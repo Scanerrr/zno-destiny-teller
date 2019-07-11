a8587bab3777d46f5bc6d89ce219e0ace5102646
Vadym <wadedevelop@gmail.com>
2019-07-11 23:23:29 +0300

    Extended config

--- config.js
+++ config.js
@@ -1,7 +1,11 @@
 const dotenv = require("dotenv");
+
 dotenv.config();
 module.exports = {
-  botApiKey: process.env.TELEGRAM_BOT_API,
+  telegram: {
+    api_key: process.env.TELEGRAM_BOT_API,
+    webhook_url: process.env.TELEGRAM_WEBHOOK_URL
+  },
   chatID: process.env.TELEGRAM_CHAT_ID,
   login: process.env.ZNO_LOGIN,
   password: process.env.ZNO_PASSWORD,
