d4d672ae71b84d5b1c806d48ff4462a9b4d23d37
Vadym <wadedevelop@gmail.com>
2019-07-11 23:22:58 +0300

    initialize_bot and update bot endpoints

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const Telegram = require("./telegram");
 
 const config = require("./config");
 
+const bot = new Telegram(config.telegram.api_key);
 const app = express();
 
 // Add body-parser middleware
@@ -9,9 +11,35 @@ app.use(require("body-parser").json());
 // Add morgan logger middleware
 app.use(require("morgan")("combined"));
 
-// Bot endpoint
-app.post("/bot", (req, res) => {
-  console.log("Received body: ", req.body);
+// Bot endpoints
+app.post("/initialize_bot", async (req, res) => {
+  // check if webhook exists already
+  const { data } = await bot.getWebhookInfo();
+
+  // let's assume that no url means hook doesn't exist
+  if (data && data.result && !data.result.url) {
+    try {
+      // set up the new hook
+      await bot.setWebhook({
+        url: config.telegram.webhook_url
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  res.send();
+});
+
+app.post("/bot", async (req, res) => {
+  const { message } = req.body;
+
+  // bot received a message
+  if (message) {
+    if (message.entities && message.entities.type === "bot_command") {
+      // bot command received
+    }
+  }
   res.send();
 });
 
