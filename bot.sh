6904ac37d3bd33777c6418b018cc9314e79c0456
Vadym <wadedevelop@gmail.com>
2019-07-11 23:23:19 +0300

    initialize_bot bash script

--- bot.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-curl -X POST -H "Content-Type: application/json" 127.0.0.1:8080/bot -d '{ "command": "hello" }'
---
+++ initialize_bot.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+curl -X POST https://79f7dbce.ngrok.io/initialize_bot
